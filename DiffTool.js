1ba43762bfaa48b8171675c80bad63121022f8da
Gabor Nadai <gabor.nadai@gmail.com>
2015-03-21 11:40:09 +0100

    JSLint things and comments

--- DiffTool.js
+++ DiffTool.js
@@ -5,48 +5,72 @@ var request = require("request");
 
 module.exports = function () {
 
+    /**
+     * Replaces the < and > characters with &lt; and &gt; strings.
+     *
+     * @param {string}  text
+     *
+     * @returns {string}
+     */
     var clear = function (text) {
         return text.replace("<", "&lt;").replace(">", "&gt;");
     };
 
+    /**
+     * Highlights the character differencies with a span element.
+     *
+     * @param {string}  left
+     * @param {string}  right
+     *
+     * @returns {string}
+     */
     var highlight = function (left, right) {
         var length = left.length || 0,
             i,
-            diff = "",
-            openPos = 0;
+            highlighted = "",
+            lastMatchingPosition = 0,
             open = false;
 
         for (i = 0; i <= length; i++) {
             if (right.hasOwnProperty(i)) {
                 if (right[i] != left[openPos]) {
                     if (!open) {
-                        diff += "<span class='diff'>";
+                        highlighted += "<span class='diff'>";
                         open = true;
-                        openPos = i;
+                        lastMatchingPosition = i;
                     }
                 } else {
                     if (0 < open) {
-                        diff += "</span>";
+                        highlighted += "</span>";
                         open = false;
                     }
-                    openPos++;
+                    lastMatchingPosition++;
                 }
-                diff += right[i];
+                highlighted += right[i];
             }
         }
 
-        return diff;
+        return highlighted;
     };
 
+    /**
+     * Collects the different lines of two given string
+     * and returns it within the given callback.
+     *
+     * @param {string}      urlLeft
+     * @param {string}      responseLeft
+     * @param {string}      urlRight
+     * @param {string}      responseRight
+     * @param {function}    callback
+     */
     var diff = function (urlLeft, responseLeft, urlRight, responseRight, callback) {
         var diff = [],
             left = responseLeft.split("\n"),
             right = responseRight.split("\n"),
             lineLeft,
             lineRight,
-            i,
-            j,
-            lines;
+            lines,
+            i;
 
         lines = (left.length > right.length ? left.length : right.length);
 
@@ -54,26 +78,33 @@ module.exports = function () {
             lineLeft = (left.hasOwnProperty(i) ? left[i] : "");
             lineRight = (right.hasOwnProperty(i) ? right[i] : "");
 
-
             if (lineLeft != lineRight) {
-
                 diff.push({
-                    line : i + 1,
-                    left : clear(lineLeft),
+                    line: i + 1,
+                    left: clear(lineLeft),
                     right: highlight(clear(lineLeft), clear(lineRight))
                 });
             }
         }
 
-        callback({
-            diff : diff,
-            left : responseLeft,
-            right : responseRight
+        callback(false, {
+            diff: diff,
+            left: responseLeft,
+            right: responseRight
         });
     };
 
+    /**
+     * Downloads the two given URL and collects
+     * the different lines, returns it within
+     * the given callback.
+     *
+     * @param {string}      urlLeft
+     * @param {string}      urlRight
+     * @param {function}    callback
+     */
     var start = function (urlLeft, urlRight, callback) {
-        request({ url : urlLeft }, function (errorLeft, responseLeft) {
+        request({ url: urlLeft }, function (errorLeft, responseLeft) {
             if (!responseLeft) {
                 callback("Failed to load LEFT URL: " + urlLeft, null);
                 return;
@@ -93,4 +124,4 @@ module.exports = function () {
     return {
         start: start
     };
-}
+};
